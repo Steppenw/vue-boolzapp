5a7bb32efceb0ed1d863b2d69cdc7ff56b2801df
Steppenw <s.pagani73@gmail.com>
2021-05-05 16:31:55 +0200

    Commit2.

--- scripts/app.js
+++ scripts/app.js
@@ -2,8 +2,16 @@ const app = new Vue({
     el: "#app",
     data: {
         chatsList: globalChatsList,
+        currentChat: {}
     },
     methods: {
         getAvatar: user => 'img/avatar' + user.avatar + '.jpg',
+
+        chatWith(user) {
+            this.currentChat = user;
+        }
+    },
+    mounted() {
+        this.currentChat = this.chatsList[2];
     }
 });
